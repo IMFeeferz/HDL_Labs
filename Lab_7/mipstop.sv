836ff32aded70ff3b44e2df3b08c18548177edc2
Tanvi Susarla <tanvimurty@gmail.com>
2024-04-24 17:22:49 -0500

    Update mipstop.sv

--- Lab_7/mipstop.sv
+++ Lab_7/mipstop.sv
@@ -6,14 +6,14 @@ module top(input  logic       clk, reset,
            output logic [31:0] writedata, adr, 
            output logic       memwrite);
 
-  logic [31:0] instr, readdata;
+  logic [31:0] readdata;
   
   // instantiate processor and memories
 
-  mips mips(clk, reset,adr, writedata,memwrite, readdata);
+  mips mips(clk, reset, adr, writedata, memwrite, readdata);
 
   //imem imem(pc[7:2], instr);
 
-  dmem dmem(clk, memwrite, adr, writedata, readdata);
+  mem mem(clk, memwrite, adr, writedata ,readdata);
 
 endmodule
