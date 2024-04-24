75e4423ba437688b3da4970e05fff24aeb2ac085
Tanvi Susarla <tanvimurty@gmail.com>
2024-04-24 17:20:57 -0500

    Update mipsmem.sv

--- Lab_7/mipsmem.sv
+++ Lab_7/mipsmem.sv
@@ -1,19 +1,21 @@
 // External memories used by MIPS single-cycle processor
 
-module dmem(input  logic        clk, we,
+module mem(input  logic        clk, we,
             input  logic [31:0] a, wd,
             output logic [31:0] rd);
 
-  logic [31:0] RAM[63:0];
-  //logic [2:0]state;
-  initial
-    begin
-     	$readmemh("memfile.dat",RAM); // initialize memory 
-    end
-   assign rd = RAM[a]; // word aligned
-   always @(posedge clk)
-    if (we)
-      RAM[a[31:2]] <= wd;
+   logic [31:0] RAM[63:0];
+
+   initial 
+	begin
+     		$readmemh("memfile_c.dat",RAM); // initialize memory 
+   	end
+
+    assign rd = RAM[a[31:2]]; // word aligned
+
+   always @(posedge clk )
+   if (we)
+        RAM[a[31:2]] <= wd;
+     
       
 endmodule
-
